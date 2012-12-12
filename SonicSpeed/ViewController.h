5fa54a05a4be0e7decb655d6e3ce53a9bfa4cbf3
enterpi <enterpi@enterpis-Mac-Pro-3.local>
2012-12-12 15:32:40 +0530

    Initial Commit

---
+++ SonicSpeed/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SonicSpeed
+//
+//  Created by enterpi on 12/12/12.
+//  Copyright (c) 2012 enterpi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SonicSpeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SonicSpeed
+//
+//  Created by enterpi on 12/12/12.
+//  Copyright (c) 2012 enterpi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
