fb2a234aa6b343356c4cef971413e625abc047ae
Recom Development <russell.jong@recomtechnologies.com>
2014-11-28 14:47:09 +0530

    commiting code for Sonic Speed
    Signed-off-by: Recom Development <russell.jong@recomtechnologies.com>

--- SonicSpeed/ViewController.h
+++ SonicSpeed/ViewController.h
@@ -9,5 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    UIImageView *sonic;
+    UISlider *slider;
+    UILabel *myLabel;
+
+
+}
+@property(nonatomic,strong)IBOutlet UIImageView *sonic;
+@property(nonatomic,strong)IBOutlet UISlider *slider;
+@property(nonatomic,strong)IBOutlet UILabel *myLabel;
+
+- (IBAction)sliderChange:(UISlider *)sender;
+
+
+
 
 @end
--- SonicSpeed/ViewController.m
+++ SonicSpeed/ViewController.m
@@ -13,9 +13,28 @@
 @end
 
 @implementation ViewController
+@synthesize sonic,myLabel,slider;
 
 - (void)viewDidLoad
 {
+    NSArray *image = [NSArray arrayWithObjects:
+                      [UIImage imageNamed:@"img1.gif"],
+                      [UIImage imageNamed:@"img2.gif"],
+                      [UIImage imageNamed:@"img3.gif"],
+                      [UIImage imageNamed:@"img4.gif"],
+                      [UIImage imageNamed:@"img5.gif"],
+                      [UIImage imageNamed:@"img6.gif"],
+                      [UIImage imageNamed:@"img7.gif"],
+                      [UIImage imageNamed:@"img8.gif"],nil];
+    
+    sonic.frame = CGRectMake(0, 0, 100, 100);
+    sonic.animationImages = image;
+    sonic.animationDuration = 0.25;
+    sonic.animationRepeatCount = 0;
+    
+    [sonic startAnimating];
+    
+                     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -26,4 +45,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+
+- (IBAction)sliderChange:(UISlider *)sender
+{
+    myLabel.text = [NSString stringWithFormat:@"slider value = %1.2f",slider.value];
+    sonic.animationDuration = slider.value;
+    [sonic startAnimating];
+    
+    
+    
+}
 @end
